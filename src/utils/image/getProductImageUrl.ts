3216ff7a85587f4dc203c471ec8d1042c9728a2c
Michail Kobcev <mkobcev@themsteam.com>
2021-10-27 12:57:50 +0700

    Amazon images url fix

--- src/utils/image/getProductImageUrl.ts
+++ src/utils/image/getProductImageUrl.ts
@@ -1,4 +1,4 @@
 import { IProduct } from 'src/types';
 
 export const getProductImageUrl = (asin: IProduct['asin']) =>
-  `http://images.amazon.com/images/P/${asin}.01._SCMZZZZZZZ_.jpg`;
+  `https://images.amazon.com/images/P/${asin}.01._SCMZZZZZZZ_.jpg`;
