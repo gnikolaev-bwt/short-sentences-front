4da0b08503acb863b58d79536217aeaf4e0ced56
Michail Kobcev <mkobcev@themsteam.com>
2021-10-29 14:17:33 +0700

    Clock loader fix

--- src/components/atoms/CenteredBox/CenteredBox.tsx
+++ src/components/atoms/CenteredBox/CenteredBox.tsx
@@ -3,8 +3,10 @@ import { FC } from 'react';
 export const CenteredBox: FC = (props) => {
   const { children } = props;
   return (
-    <div className='w-100 h-100 d-flex flex-column align-items-center p-5'>
-      <div className='text-center'>{children}</div>
+    <div className='w-100 h-100 p-5'>
+      <div className='d-flex flex-column align-items-center text-center'>
+        {children}
+      </div>
     </div>
   );
 };
